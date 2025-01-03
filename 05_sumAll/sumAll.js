5b1356098eb172b8b45a8ddeca53ab8c13301a4f
auksztol <bigiottini@gmail.com>
2025-01-02 16:34:03 -1000

    Add solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start == "number" && 
+        Number.isInteger(start) && 
+        start > 0 &&
+        typeof end == "number" && 
+        Number.isInteger(end) && 
+        end > 0) {
+        sumArray = [];
+        if (start < end) {
+            for (i = start; i <= end; i++) {
+                sumArray.push(i);
+            }
+        } else {
+            for (i = end; i <= start; i++) {
+                sumArray.push(i);
+            }
+        }
+        let sum = 0;
+        for (let x of sumArray) {
+            sum += x;
+        }
+        return sum;
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
